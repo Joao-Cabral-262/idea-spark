778b5c68c0ac2aa2afab1ae794b9bf5e38e1c280
João Pedro Cabral Machado <joao.cabral@autojun.com.br>
2026-05-22 22:04:41 -0300

    feat: atualiza roteador com configurações locais

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: '/idea-spark',
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
