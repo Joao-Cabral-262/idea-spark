280590fdf1370aa9e10af90ab6ac0d9e773b8c30
João Pedro Cabral Machado <joao.cabral@autojun.com.br>
2026-05-22 23:09:31 -0300

    Updates

--- server/assets/_tanstack-start-manifest_v-BJ2AvBgV.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Usuario/.vscode/idea-spark/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BkzrwjAi.js"] }, "/": { filePath: "C:/Users/Usuario/.vscode/idea-spark/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CR55c3Im.js"] } }, clientEntry: "/assets/index-BkzrwjAi.js" });
-export {
-  tsrStartManifest
-};
--- server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-DzVZhHcv.js";
-import "node:events";
-export {
-  w as default
-};
